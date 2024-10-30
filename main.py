db856a03953d7255637dbd322920c75375e42f1d
Intern-Wrexa <wrexaintern@gmail.com>
2024-10-30 21:36:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,96 @@
-from typing import Optional
+import httpx
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import HTMLResponse
+import json
 
-from fastapi import FastAPI
 
 app = FastAPI()
 
-
+# Root function
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    html_content = """
+    <html>
+        <head>
+            <style>
+                body {
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    height: 100vh;
+                    margin: 0;
+                }
+                h1 {
+                    font-size: 48px;
+                    color: #333;
+                }
+            </style>
+        </head>
+        <body>
+            <h1>Api Made By D</h1>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content)
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+# Get property details for old bill
+@app.get("/fetch-water-bill/old-bill/")
+async def fetch_old_water_bill(zone: str, ward: str, bill: str, sub: str, total: bool = False):
+    url = "https://bnc.chennaimetrowater.in/svr/bnc-ms.php?getCustomer=true"
+
+    # JSON payload
+    payload = {
+        "old_zone_no": zone,
+        "old_ward_no": ward,
+        "old_bill_no": bill,
+        "old_sub_code": sub,
+        "include_total_dues": total
+    }
+
+    async with httpx.AsyncClient() as client:
+        response = await client.post(url, json=payload)
+
+    if response.status_code == 200:
+        response_data = response.json()
+
+        # Pretty-print the JSON response
+        formatted_json = json.dumps(response_data, indent=4)
+        return formatted_json
+
+        # or return the json directly
+        #return (response.json())
+
+    else:
+        raise HTTPException(status_code=response.status_code, detail="Failed to fetch details")
+
+# Get property details for new bill
+@app.get("/fetch-water-bill/new-bill/")
+async def fetch_new_water_bill(zone: str, ward: str, bill: str, total: bool = False):
+    url = "https://bnc.chennaimetrowater.in/svr/bnc-ms.php?getCustomer=true"
+
+    # JSON payload
+    payload = {
+        "zone_no": zone,
+        "ward_no": ward,
+        "bill_no": bill,
+        "include_total_dues": total
+    }
+
+    async with httpx.AsyncClient() as client:
+        response = await client.post(url, json=payload)
+
+    if response.status_code == 200:
+        response_data = response.json()
+
+        # Pretty-print the JSON response
+        formatted_json = json.dumps(response_data, indent=4)
+        return formatted_json
+
+        # or return the json directly
+        #return (response.json())
+
+    else:
+        raise HTTPException(status_code=response.status_code, detail="Failed to fetch details")
+
+
+
